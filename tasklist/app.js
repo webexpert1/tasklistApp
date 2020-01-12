f6cea9a9bf2b5a6eac25edfcc2982a8db5fd33e3
Richard <richokoye1@gmail.com>
2020-01-12 12:30:53 +0100

    add local storage

--- tasklist/app.js
+++ tasklist/app.js
@@ -9,6 +9,8 @@ const taskInput = document.querySelector('#task');
 loadEventListners();
 
 function loadEventListners() {
+    // load from ls
+    document.addEventListener('DOMContentLoaded', getTasks);
     // Add task event
     form.addEventListener('submit', AddTask);
     // Remove task event
@@ -40,27 +42,97 @@ function AddTask(e) {
 
     taskList.appendChild(li);
 
+    // store in ls
+    StoreTaskInLocalStorage(taskInput.value);
     // clear input field
     taskInput.value = '';
 
-    console.log(li)
     e.preventDefault()
 }
 
+
+// store task in Local storage
+function StoreTaskInLocalStorage(task) {
+    let tasks;
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+}
+
+// get tasks from ls
+function getTasks() {
+    let tasks;
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    
+    tasks.forEach(function(task) {
+        // create li
+        const li = document.createElement('li');
+        // Add class
+        li.className = 'collection-item';
+        // create a text node and append to li
+        li.appendChild(document.createTextNode(task));
+        // Create  new link element
+        const link = document.createElement('a');
+        // add class
+        link.className = "delete-item secondary-content";
+        // add icon
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        //append the link to li
+        li.appendChild(link);
+
+        taskList.appendChild(li);
+    })
+
+}
 // remove task
 function RemoveTask(e) {
     if(e.target.parentNode.classList.contains('delete-item')) {
         e.target.parentNode.parentNode.remove();
 
+        // remove from ls
+        removeTaskFromLocalStorage(e.target.parentNode.parentNode);
     }
     
 }
 
+// remove from ls
+function removeTaskFromLocalStorage(taskItem) {
+    let tasks;
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task, index) {
+        if(taskItem.textContent === task) {
+            tasks.splice(index, 1)
+        }
+    })
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
 // clear all tasks
 function ClearTasks(e) {
     console.log(taskList.firstChild)
     // taskList.innerHTML = '';
-    taskList.removeChild(taskList.firstChild)
+    taskList.removeChild(taskList.firstChild);
+
+    // clear from LS
+    clearTasksFromLocalStorage();
+}
+
+// clear tasks from ls
+function clearTasksFromLocalStorage() {
+    localStorage.clear();
 }
 
 // Filter tasks
